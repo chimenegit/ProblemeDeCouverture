8dfd96d008be78879095c47476561d8e351e2508
chimenegit <39130849+chimenegit@users.noreply.github.com>
2021-08-02 21:51:35 +0000

    Update toulensemble.py

--- toulensemble.py
+++ toulensemble.py
@@ -43,7 +43,7 @@ df=df.fillna(0)
 
 # LA TAILLE DE DONNEES CONSIDEREES
 
-dh=df.loc[35001:40000]#head(5000)#
+dh=df.loc[40001:45000]#head(5000)#
 data=dh.values.tolist()
 
 data_int=[]
@@ -802,7 +802,7 @@ finally:
           for o in range(n+1, len(List_datJou)):
                       list_coupledatJou.append(set([List_datJou[n], List_datJou[o]]))
 
-      Couple_Essai={12487, 12875}
+      Couple_Essai={12355, 12783}
       compteDi=list_coupDi.count(Couple_Essai)
       compteJou=list_coupledatJou.count(Couple_Essai)
       print(compteDi, compteJou)
@@ -828,7 +828,7 @@ finally:
                       list_coupledatIDSA.append(set([List_datIDSA[n], List_datIDSA[o]]))
 
 
-      Couple_Essai={12487, 12875}
+      Couple_Essai={12355, 12783}
       compteDi=list_coupDi.count(Couple_Essai)
       compteIDSA=list_coupledatIDSA.count(Couple_Essai)
       print(compteDi, compteIDSA)
@@ -852,7 +852,7 @@ finally:
           for o in range(n+1, len(List_dat)):
                       list_coupleIDGC.append(set([List_dat[n], List_dat[o]]))
 
-      Couple_Essai={12487, 12875}
+      Couple_Essai={12355, 12783}
       compteDi=list_coupDi.count(Couple_Essai)
       compteIDGC=list_coupleIDGC.count(Couple_Essai)
       print(compteIDGC, compteDi)
