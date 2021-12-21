cc151ff94fd09d161dba6c375f51e85261534953
chimenegit <39130849+chimenegit@users.noreply.github.com>
2021-12-21 19:23:36 +0000

    Update toulensemble.py

--- toulensemble.py
+++ toulensemble.py
@@ -3,8 +3,8 @@
 
 # In[13]:
 
-#import sys
-#sys.setrecursionlimit(1500)
+import sys
+sys.setrecursionlimit(1500000)
 import pandas as pd
 import numpy as np
 import operator
@@ -44,7 +44,7 @@ df=df.fillna(0)
 
 # LA TAILLE DE DONNEES CONSIDEREES
 
-dh=df.head(2000)#loc[54001:59601]
+dh=df.head(30000)#loc[54001:59601]
 data=dh.values.tolist()
 
 data_int=[]
