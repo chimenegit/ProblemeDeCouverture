fff68ef53ab6f82bc7e8c0e1569ce6131f005f00
chimenegit <39130849+chimenegit@users.noreply.github.com>
2021-12-28 08:24:41 +0000

    Update toulensemble.py

--- toulensemble.py
+++ toulensemble.py
@@ -880,8 +880,7 @@ finally:
           for o in range(n+1, len(List_dat)):
                       list_coupleIDGC.append(set([List_dat[n], List_dat[o]]))
       Couple_Essai={55267, 55271}
-      #Couple_Essai={12703, 10315}
-      #compteDi=list_coupDi.count(Couple_Essai)
+      compteDi=list_coupDi.count(Couple_Essai)
       compteIDGC=list_coupleIDGC.count(Couple_Essai)
       #print(compteDi, compteIDGC)
 
