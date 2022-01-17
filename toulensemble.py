07ee9ef13ac904d3764b9b1afb32aa5f7b8c611f
chimenegit <39130849+chimenegit@users.noreply.github.com>
2022-01-17 15:59:04 +0000

    Update toulensemble.py

--- toulensemble.py
+++ toulensemble.py
@@ -44,7 +44,7 @@ df=df.fillna(0)
 
 # LA TAILLE DE DONNEES CONSIDEREES
 
-dh=df.loc[10001:15000]  # head(5000)#
+dh=df.loc[15001:20000]  # head(5000)#
 data=dh.values.tolist()
 
 data_int=[]
@@ -881,7 +881,7 @@ finally:
           for o in range(n+1, len(List_datJou)):
                       list_coupledatJou.append(set([List_datJou[n], List_datJou[o]]))
 
-      Couple_Essai={55267, 55271}
+      Couple_Essai={55331, 55335}
       compteDi=list_coupDi.count(Couple_Essai)
       compteJou=list_coupledatJou.count(Couple_Essai)
       print("Compt_Ajout:",compteDi, compteJou)
